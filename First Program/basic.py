67a53b660cf1eca8920eb6fd405889ded4ec4796
eyahiya-dev <eyahiya.dev@gmail.com>
2023-10-19 20:15:19 +0600

    add python program

--- First Program/basic.py
+++ First Program/basic.py
@@ -1,1 +1,15 @@
-print("Hello, Welcome to the world of Python Programming")
+# Basic of Python Programming
+print("Hello World")
+
+# simple if else conditoins
+if 5 > 7:
+    print("Five is greater than Two")
+else:
+    print("Invalid")
+    
+# variable
+x = 10
+y = 15
+z = x + y
+print(z)
+
